8588a29bc23eaaeef9b917a525f5becfb31d1d64
Rafos2423 <vladp2003kek@gmail.com>
2022-09-27 21:00:57 +0300

    1

--- practice1/practice.py
+++ practice1/practice.py
@@ -1,16 +1,21 @@
 import random
-a = [0, 0, 1]
-count = 0
-for i in range(100000):
-    choice = random.randint(0, 2)
-    for i in range(2):
-        if (a[i] == 0 and i != choice):
-            choiseLead = i;
-            break
-    if (choice == 1):
-        count += 1;
 
-print(count/1000)
+agree = 0
+not_agree = 0
+
+for i in range(100000):
+    a = [0, 0, 1]
+
+    choice = random.randint(0, 2)
+    a.pop(choice)
+    a.remove(0)
+
+    if a[0] == 1:
+        agree += 1
+    else:
+        not_agree += 1
+
+print(f'agree: {agree/1000}\nnot agree: {not_agree/1000}')
 
 
 
