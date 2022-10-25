d70d8617ca5ef4110da341964398dbd68bc0765d
Rafos2423 <vladp2003kek@gmail.com>
2022-10-25 12:15:21 +0300

    -

--- practice/practice4/practice.py
+++ practice/practice4/practice.py
@@ -14,12 +14,14 @@ def save_file(name, words):
     f = open(name, 'r')
     text = f.read()
     if len(text) != 0:
-        print('Обнаружены данные. Очистите файл')
+        f.seek(0)
+        f.close()
         return 0
 
     f = open(name, 'a')
     f.write(f'Всего уникальных слов: {str(len(words))} \n================\n')
     f.write('\n'.join(words))
+    f.close()
 
 words = read_file('data.txt')
 save_file("count.txt", words)
