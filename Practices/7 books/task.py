3cc6fe05eb84b5678f54ceeda92c4d386817dc8c
Rafos2423 <vladp2003kek@gmail.com>
2022-12-13 23:47:29 +0300

    -

--- Practices/7 books/task.py
+++ Practices/7 books/task.py
@@ -5,13 +5,31 @@ def get_books(filename: str, name: str):
     with open(filename, 'r') as f:
         reader = csv.reader(f, delimiter='|')
 
-        result = list
+        result = [[]]
+        result.clear()
+
         for row in reader:
-            if row.__contains__(name):
+            if ''.join(row).__contains__(name):
                 result.append(row)
 
         return result
 
+def get_totals(data: [[]], sum = 0, add = 0):
+    result = [[]]
+    result.clear()
 
-books = get_books("file.csv", "python")
-print(books)
+    for row in data:
+        count = int(row[3]) * float(row[4])
+        if count < sum:
+            sum += add
+        result.append([row[0], count])
+
+    return result
+
+
+books = get_books("file.csv", "Python")
+result = get_totals(books)
+
+for i in result:
+    print(i)
+
