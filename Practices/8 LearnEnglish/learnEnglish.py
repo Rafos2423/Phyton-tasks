953b47f4506ef44383a27c41ff5852d851901eba
Rafos2423 <vladp2003kek@gmail.com>
2022-12-14 19:59:33 +0300

    -

--- Practices/8 LearnEnglish/learnEnglish.py
+++ Practices/8 LearnEnglish/learnEnglish.py
@@ -1,6 +1,7 @@
 import re
 import pymorphy2
 from translate import Translator
+import translate
 
 def read_file(filename: str):
     f = open(filename, 'r', encoding='utf-8')
@@ -13,44 +14,61 @@ def list_words(words: [str]):
     wordsCount = {}
 
     for word in words:
-        if not word in wordsCount:
-            wordsCount[word] = 0
-        wordsCount[word] += 1
+        if len(word) > 1:
+            if not word in wordsCount:
+                wordsCount[word] = 0
+            wordsCount[word] += 1
 
-    sorted_dict = {}
+    sorted_words = {}
     sorted_keys = sorted(wordsCount, key=wordsCount.get)
     sorted_keys.reverse()
 
     for values in sorted_keys:
-        sorted_dict[values] = wordsCount[values]
+        sorted_words[values] = wordsCount[values]
 
-    return sorted_dict
+    return sorted_words
 
 def translate(words: {}):
     morph = pymorphy2.MorphAnalyzer()
-    translator = Translator(from_lang="russian", to_lang="english")
+    translator = Translator(from_lang="ru", to_lang="en")
 
     translate_words = {}
     for key, value in words.items():
-        normal_word = morph.parse(key)[0].normal_form
-        en_word = translator.translate(normal_word)
-        translate_words[en_word] = value
+        en_word = str
+        try:
+            normal_word = morph.parse(key)[0].normal_form
+            en_word = translator.translate(normal_word)
+            if en_word == None:
+                raise Exception
+        except:
+            en_word = 'untranslated'
+
         print(en_word)
+        translate_words[en_word] = value
 
     return translate_words
 
 
 def write_file(filename, translate_words: {}, sorted_words: {}):
-    f = open(filename)
+    ru_words = []
+    en_words = []
+    words_count = []
 
-    for i in range(len(sorted_words)):
-        line = f'{translate_words.keys()[i]}, {sorted_words.keys()[i]} - {sorted_words[i]}'
-        print(line)
+    for key, value in sorted_words.items():
+        ru_words.append(key)
+
+    for key, value in translate_words.items():
+        en_words.append(key)
+        words_count.append(value)
+
+    f = open(filename, 'a')
+    for i in range(len(translate_words)):
+        line = "{0} - {1} {2} \n".format(en_words[i], ru_words[i], words_count[i])
+        f.write(line)
+
+
 
 words = read_file('dialog.txt')
 sorted_words = list_words(words)
 transleted_words = translate(sorted_words)
 write_file('TranslatedWords.txt', transleted_words, sorted_words)
-
-print(sorted_words)
-print(transleted_words)
