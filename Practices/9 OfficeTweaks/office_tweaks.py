3242bd3fa91371b19e875e22b4b2be0d2cef9d8f
Rafos2423 <vladp2003kek@gmail.com>
2022-12-18 15:14:48 +0300

    -

--- Practices/9 OfficeTweaks/office_tweaks.py
+++ Practices/9 OfficeTweaks/office_tweaks.py
@@ -13,7 +13,7 @@ def correct_input(min_value, max_value):
     return correct_input(min_value, max_value)
 
 def menu():
-    print('\nВыберите действие\n'
+    print('\nВыберите действие:\n'
               '0 - Сменить рабочий каталог\n'
               '1 - Преобразовать PDF в Docx\n'
               '2 - Преобразовать Doc, Docx в PDF\n'
@@ -48,27 +48,62 @@ def find_files(dir, format):
 
     return files
 
-def choose_files(files, format):
-    print('\nВыберите файл для конвертации (0-для всех файлов):')
+def choose_files(files):
+    print('\nВыберите файл для конвертации или нажмите 0 - для конвертации всех файлов):')
     file_num = correct_input(0, len(files))
-
     if file_num == 0:
         for file in files:
-            convert_file(file, format)
+            convert_file(file)
     else:
-        convert_file(files[file_num - 1], format)
+        convert_file(files[file_num - 1])
 
-def convert_file(file, format):
-    new_format_file = file.replace(file.split('.')[-1], format)
-    try:
-        parse(file, new_format_file)
-    except:
-        print(f'Невозможно преобразовать файл {file}')
+def convert_file(file):
+    format_file = file.split('.')[-1]
+
+    if format_file == 'pdf':
+        new_file = file.replace(format_file, 'docx')
+
+        try:
+            parse(file, new_file)
+        except:
+            print(f'Невозможно преобразовать файл {file} в формат docx')
+
+    elif format_file == 'doc' or format_file == 'docx':
+        new_file = file.replace(format_file, 'pdf')
+
+        try:
+            convert(file, new_file)
+        except:
+            print(f'Невозможно преобразовать файл {file} в формат pdf')
+
+    else:
+        print('Укажите параметры сжатия в %:')
+        quality = correct_input(1, 100)
+        try:
+            image_path = file
+            image_file = Image.open(image_path)
+            image_file.save(file, quality=quality)
+        except:
+            print(f'Невозможно сжать изображение')
 
 
-#def compress_img(img):
+def change(dir, format):
+    files = find_files(dir, format)
+    if len(files) == 0:
+        print(f'В папке нет файлов формата {", ".join(format)}')
+        return
+
+    choose_files(files)
 
 
+def menu_delete():
+    print('\nВыберите действие:\n'
+          '1 - Удалить все файлы начинающиеся с подстроки\n'
+          '2 - Удалить все файлы заканчивающиеся на подстроку\n'
+          '3 - Удалить все файлы содержащие подстроку\n'
+          '4 - Удалить все файлы по расширению')
+
+    return correct_input(1, 4)
 
 
 catalog = os.getcwd()
@@ -82,24 +117,60 @@ while True:
         print(f'Текущий каталог: {catalog}')
 
     if option == 1:
-        pdf_files = find_files(catalog, ['pdf'])
-        if len(pdf_files) == 0:
-            print(f'В папке нет файлов формата pdf')
-            continue
-
-        choose_files(pdf_files, 'docx')
+        change(catalog, ['pdf'])
 
     if option == 2:
-        docx_files = find_files(catalog, ['doc', 'docx'])
-        if len(docx_files) == 0:
-            print(f'В папке нет файлов формата doc, docx')
-            continue
-
-        choose_files(docx_files, 'pdf')
+        change(catalog, ['doc', 'docx'])
 
     if option == 3:
-        image_file = find_files(catalog, ['jpeg', 'gif', 'png', 'jpg', 'img'])
+        change(catalog, ['img', 'png', 'jpg', 'jpeg', 'bmp'])
 
+    if option == 4:
+        delete_option = menu_delete()
+        sub_string = input('Введите подстроку: ')
+
+        files = []
+        for file in os.listdir(catalog):
+            if os.path.isfile(file):
+                files.append(file)
+
+        delete_files = []
+
+        if delete_option == 1:
+            for file in files:
+                if file[0:len(sub_string)] == sub_string:
+                    delete_files.append(file)
+
+        if delete_option == 2:
+            for file in files:
+                filename = file.split('.')[0]
+                if filename[-len(sub_string):] == sub_string:
+                    delete_files.append(file)
+
+        if delete_option == 3:
+            for file in files:
+                filename = file.split('.')[0]
+                if filename.__contains__(sub_string):
+                    delete_files.append(file)
+
+        if delete_option == 4:
+            for file in files:
+                format = file.split('.')[1]
+                if format == sub_string:
+                    delete_files.append(file)
+
+        print(f'Вы действительно хотите удалить файлы(да/нет)?')
+        print("\n".join(delete_files))
+
+        while True:
+            answer = input()
+            if answer == 'да':
+                for file in delete_files:
+                    os.remove(file)
+                print(f'Файлы успешно удалены')
+                break
+            elif answer == 'нет':
+                break
 
     if option == 5:
         break
